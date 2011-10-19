9e8339c8ff11a2af736ecf90cdbc49159aa2e0ba
Matthias Tilsner <matthias@tilsner.eu>
2011-10-19 13:08:18 +0200

    Naiive implementation of a Specialization Picker
    Each Talent requires a number of specializations. These can now be added
    using a generic textfield that will autoresize.

--- grails-app/taglib/uts/chargen/WidgetsTagLib.groovy
+++ grails-app/taglib/uts/chargen/WidgetsTagLib.groovy
@@ -4,6 +4,10 @@ class WidgetsTagLib {
 	static namespace = "widgets"	
 	static spinBoxId = 0
 	
+	def resources = { attrs ->
+		out << r.require(module: "widgets")
+	}
+	
 	def numberWithBonus = { attrs ->
 		if(!attrs.value) attrs.value = 0
 		if(!attrs.bonus) attrs.bonus = 0
@@ -13,5 +17,32 @@ class WidgetsTagLib {
 					    class:"${attrs.class ? atts.class : ''} bonus numberWithBonus", type:"text", readonly:"readonly", 'data-updates':"#${attrs.id}-summe")
 		out << g.field(id:"${attrs.id}-summe", name:"${attrs.name}.summe", value:attrs.value+attrs.bonus,
 					   class:"${attrs.class ? atts.class : ''} summe numberWithBonus", type:"text", readonly:"readonly")
+	}
+	
+	def suggestiveMultipleInput = { attrs ->
+		def autocompleteJsSource = attrs.suggestions.collect({'"'+it.name+'"'})
+		out << r.script { """
+			\$("#${attrs.id}").bind("keydown", function( event ) {
+				if(event.keyCode === \$.ui.keyCode.TAB && \$(this).data("autocomplete").menu.active)
+					event.preventDefault();
+			}).autocomplete({
+				minLength: 0,
+				source: function( request, response ) {
+					response(\$.ui.autocomplete.filter(${autocompleteJsSource}, extractLast(request.term)));
+				},
+				focus: function() {
+					return false;
+				},
+				select: function(event, ui) {
+					var terms = split( this.value );
+					terms.pop();
+					terms.push( ui.item.value );
+					terms.push( "" );
+					this.value = terms.join( ", " );
+					return false;
+				}
+			}).autoResize();
+		""" }
+		out << g.textArea(id:attrs.id, name:attrs.name, value:attrs.value, class:(attrs.class ? attrs.class : ''), placeholder: "${attrs.suggestions.join(", ")}")
 	}
 }
---
+++ test/unit/uts/chargen/JQueryUITagLibTests.groovy
@@ -0,0 +1,17 @@
+package uts.chargen
+
+import grails.test.*
+
+class JQueryUITagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
